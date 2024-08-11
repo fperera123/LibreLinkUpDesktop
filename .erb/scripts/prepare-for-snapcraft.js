664a816d8b4e448c7e76b41aa960692547fbaf75
fperera123 <mrlive119@ymail.com>
2024-08-11 06:37:31 +0530

    Update package.json and prepare-for-snapcraft.js

--- .erb/scripts/prepare-for-snapcraft.js
+++ .erb/scripts/prepare-for-snapcraft.js
@@ -1,8 +1,14 @@
 // after workaround to update electron
 // this file is used to correct main entry for snapcraft build only
-const fs = require('fs');
-const path = require('path');
-const packageJson = require('./../../package.json');
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import packageJson from './../../package.json' assert { type: "json" };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const packageJsonPath = path.join(__dirname, './../../package.json');
 
