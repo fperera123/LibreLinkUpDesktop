9cc7521b5ff467d8d8e0b46488139438fca7ccca
fperera123 <mrlive119@ymail.com>
2024-09-06 13:02:54 +0530

    update: add text shadow when overlay transparent

--- src/renderer/pages/dashboard.tsx
+++ src/renderer/pages/dashboard.tsx
@@ -4,10 +4,8 @@ import { useAuthStore } from '@/stores/auth';
 import { getCGMData } from '@/lib/linkup';
 import { TrendArrow } from '@/components/ui/trend-arrow';
 import {
-  ArrowTopRightIcon,
   EnterFullScreenIcon,
   GearIcon,
-  OpenInNewWindowIcon,
 } from '@radix-ui/react-icons';
 import { useNavigate } from 'react-router-dom';
 import { LoadingScreen } from '@/components/ui/loading';
@@ -135,11 +133,29 @@ export default function DashboardPage() {
           onClick={() => changeToWindowedMode()}
           className="absolute 2xs:top-2 2xs:right-2 md:top-5 md:right-5 right-0 top-0 outline-none hover:bg-white/20 p-2 rounded-md transition-all no-draggable"
         >
-          <EnterFullScreenIcon className="text-white 2xs:h-6 2xs:w-6 w-4 h-4" />
+          <div className=''>
+          {/* <EnterFullScreenIcon className="text-white 2xs:h-6 2xs:w-6 w-4 h-4 outline-1" /> */}
+          {/* Use a custom svg icon to just to add a shadow to the icon */}
+          <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="text-white 2xs:h-6 2xs:w-6 w-4 h-4">
+            <defs>
+              <filter id="pathShadow" x="-50%" y="-50%" width="200%" height="200%">
+                <feDropShadow dx="2" dy="1" stdDeviation="2" flood-color="rgba(0, 0, 0, 0.9)"></feDropShadow>
+              </filter>
+            </defs>
+
+            <path d="M2 2.5C2 2.22386 2.22386 2 2.5 2H5.5C5.77614 2 6 2.22386 6 2.5C6 2.77614 5.77614 3 5.5 3H3V5.5C3 5.77614 2.77614 6 2.5 6C2.22386 6 2 5.77614 2 5.5V2.5ZM9 2.5C9 2.22386 9.22386 2 9.5 2H12.5C12.7761 2 13 2.22386 13 2.5V5.5C13 5.77614 12.7761 6 12.5 6C12.2239 6 12 5.77614 12 5.5V3H9.5C9.22386 3 9 2.77614 9 2.5ZM2.5 9C2.77614 9 3 9.22386 3 9.5V12H5.5C5.77614 12 6 12.2239 6 12.5C6 12.7761 5.77614 13 5.5 13H2.5C2.22386 13 2 12.7761 2 12.5V9.5C2 9.22386 2.22386 9 2.5 9ZM12.5 9C12.7761 9 13 9.22386 13 9.5V12.5C13 12.7761 12.7761 13 12.5 13H9.5C9.22386 13 9 12.7761 9 12.5C9 12.2239 9.22386 12 9.5 12H12V9.5C12 9.22386 12.2239 9 12.5 9Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd" filter="url(#pathShadow)"></path>
+          </svg>
+          </div>
         </button>
       )}
       <div className="flex items-center gap-3">
-        <p className="text-white font-semibold xs:text-3xl text-xl">
+        <p
+            className={`${
+              currentWindowMode === 'overlayTransparent'
+                ? 'overlay-shadow'
+                : ''
+            } text-white font-semibold xs:text-3xl text-xl`}
+        >
           {getUserValue(graphData?.glucoseMeasurement?.ValueInMgPerDl) +
             ' ' +
             getUserUnit()}
